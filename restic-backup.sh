4b88e43101fa6dd0750b37724dcba91e0900209e
Yannis Huber <yannis.huber@hefr.ch>
2022-09-05 16:43:41 +0200

    feat: add logging of the backup output

--- restic-backup.sh
+++ restic-backup.sh
@@ -6,6 +6,8 @@ source /users/yannis/.restic-config
 
 PID_FILE=~/.restic_backup.pid
 TIMESTAMP_FILE=~/.restic_backup_timestamp
+TEMP_LOG_FILE=~/.restic_temp_log
+LOG_FILE=~/.restic_log
 
 if [ -f "$PID_FILE" ]; then
   if ps -p $(cat $PID_FILE) > /dev/null; then
@@ -38,14 +40,14 @@ echo $(date +"%Y-%m-%d %T") "Backup start"
 ERRORS=0
 
 # Backup Document folder
-/opt/homebrew/bin/restic backup --host=mbp-yhu /users/yannis/documents --exclude-file=/users/yannis/.restic-exclude
+/opt/homebrew/bin/restic backup --host=mbp-yhu /users/yannis/documents --exclude-file=/users/yannis/.restic-exclude >> $TEMP_LOG_FILE 2>&1
 
 if [[ $? -ne 0 ]]; then 
   ERRORS=1
 fi
 
 # Backup Pictures folder
-/opt/homebrew/bin/restic backup --host=mbp-yhu /users/yannis/pictures --exclude-file=/users/yannis/.restic-exclude
+/opt/homebrew/bin/restic backup --host=mbp-yhu /users/yannis/pictures --exclude-file=/users/yannis/.restic-exclude >> $TEMP_LOG_FILE 2>&1
 
 if [[ $? -ne 0 ]]; then 
   ERRORS=1
@@ -53,16 +55,19 @@ fi
 
 
 # Prune old backups
-/opt/homebrew/bin/restic forget --keep-daily 7 --keep-weekly 5 --keep-monthly 12 --keep-yearly 75 --prune
+/opt/homebrew/bin/restic forget --keep-daily 7 --keep-weekly 5 --keep-monthly 12 --keep-yearly 75 --prune >> $TEMP_LOG_FILE 2>&1
 
 if [[ $? -ne 0 ]]; then 
   ERRORS=1
 fi
 
-curl --retry 3 https://hc-ping.com/$(security find-generic-password -s restic-backup-healthcheck-uuid -w)/$ERRORS
+cat $TEMP_LOG_FILE >> $LOG_FILE
+
+cat $TEMP_LOG_FILE | curl --retry 3 -X POST --data-binary "@-" https://hc-ping.com/$(security find-generic-password -s restic-backup-healthcheck-uuid -w)/$ERRORS
 
 echo $(date +"%Y-%m-%d %T") "Backup finished"
 echo $(date -v +8H +"%s") > $TIMESTAMP_FILE
 
 rm $PID_FILE
+rm $TEMP_LOG_FILE
 
